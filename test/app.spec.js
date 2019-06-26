5004f260a844950bfabf073ebe1e988e1e228c20
ryan6921 <rmkaw@asu.edu>
2019-06-25 19:59:59 -0700

    first commit

---
+++ test/app.spec.js
@@ -0,0 +1,10 @@
+
+const app = require('../src/App')
+
+describe('App', () => {
+  it('GET / responds with 200 containing "Hello, world!"', () => {
+    return supertest(app)
+      .get('/')
+      .expect(200, 'Hello, world!')
+  })
+})
